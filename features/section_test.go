beaa510b3e0b5c95ad1cee16788f260def49c1da
Ernie Billing <ebilling@pivotal.io>
2019-07-17 12:01:58 -0700

    completed section end marker

--- features/section_test.go
+++ features/section_test.go
@@ -27,6 +27,15 @@ var _ = Describe("log section boundaries", func() {
 		steps.And("the result is a machine and human readable section begin line")
 	})
 
+	Scenario("logging end of a section", func() {
+		steps.Given("I have the mrlog binary")
+
+		steps.When("I log a section end")
+
+		steps.Then("the command exits without error")
+		steps.And("the result is a machine and human readable section end line")
+	})
+
 	steps.Define(func(define Definitions) {
 		var (
 			commandSession *gexec.Session
@@ -54,13 +63,26 @@ var _ = Describe("log section boundaries", func() {
 			Expect(err).NotTo(HaveOccurred())
 		})
 
+		define.When(`^I log a section end`, func() {
+			logCommand := exec.Command(
+				mrlogPath,
+				"section-end",
+				"--result",
+				"1",
+			)
+
+			var err error
+			commandSession, err = gexec.Start(logCommand, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
 		define.Then(`^the command exits without error$`, func() {
 			Eventually(commandSession).Should(gexec.Exit(0))
 		})
 
 		define.Then(`^the result is a machine and human readable section begin line$`, func() {
 			Eventually(commandSession.Out).Should(
-				Say("section-start: test-section"))
+				Say("section-start: 'test-section'"))
 
 			contents := commandSession.Out.Contents()
 
@@ -70,9 +92,9 @@ var _ = Describe("log section boundaries", func() {
 			machineReadableMatches := mrRE.FindSubmatch(contents)
 
 			machineReadable := &struct {
-				Type    string `json:"type"`
-				Name    string `json:"name"`
-				Time    time.Time
+				Type string `json:"type"`
+				Name string `json:"name"`
+				Time time.Time
 			}{}
 
 			err := json.Unmarshal(machineReadableMatches[1], machineReadable)
@@ -80,6 +102,31 @@ var _ = Describe("log section boundaries", func() {
 
 			Expect(machineReadable.Type).To(Equal("section-start"))
 			Expect(machineReadable.Name).To(Equal("test-section"))
+			Expect(machineReadable.Time.Unix()).To(BeNumerically("~", time.Now().Unix(), 2))
+		})
+
+		define.Then(`^the result is a machine and human readable section end line$`, func() {
+			Eventually(commandSession.Out).Should(
+				Say("section-end: result: 1"))
+
+			contents := commandSession.Out.Contents()
+
+			mrRE := regexp.MustCompile(`\s(?m)MRL:(.*)\n`)
+			Expect(mrRE.Match(contents)).To(BeTrue())
+
+			machineReadableMatches := mrRE.FindSubmatch(contents)
+
+			machineReadable := &struct {
+				Type   string `json:"type"`
+				Result int    `json:"result"`
+				Time   time.Time
+			}{}
+
+			err := json.Unmarshal(machineReadableMatches[1], machineReadable)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(machineReadable.Type).To(Equal("section-end"))
+			Expect(machineReadable.Result).To(Equal(1))
 			Expect(machineReadable.Time.Unix()).To(BeNumerically("~", time.Now().Unix(), 2))
 		})
 
