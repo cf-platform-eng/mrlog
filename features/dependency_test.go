eda878ac439b1f6deef9a3e89b59ecfd89c16aa2
Serafima Ostrovskaya <sostrovskaya@pivotal.io>
2019-09-04 15:16:03 -0400

    Backfill and clean tests for `dependency` feature
    [#167656360]

--- features/dependency_test.go
+++ features/dependency_test.go
@@ -24,16 +24,35 @@ var _ = Describe("log a dependency", func() {
 		steps.When("I log a dependency")
 
 		steps.Then("the command exits without error")
-		steps.And("the result is a machine and human readable dependency log line")
+		steps.And("the result contains a human readable log")
+		steps.And("the result contains a machine readable log")
 	})
 
-	Scenario("logging a dependency with JSON metadata", func() {
+	Scenario("logging a dependency with metadata", func() {
 		steps.Given("I have the mrlog binary")
 
-		steps.When("I log a dependency with JSON metadata")
+		steps.When("I log a dependency with metadata")
 
 		steps.Then("the command exits without error")
-		steps.And("the resulting machine readable dependency log line contains provided metadata")
+		steps.And("the result contains a human readable log")
+		steps.And("the result contains a machine readable log")
+		steps.And("the machine readable dependency log contains provided metadata")
+	})
+
+	Scenario("logging a dependency without a name", func() {
+		steps.Given("I have the mrlog binary")
+
+		steps.When("I log a dependency without a name")
+
+		steps.Then("the command exits with an error")
+	})
+
+	Scenario("logging a dependency without a version", func() {
+		steps.Given("I have the mrlog binary")
+
+		steps.When("I log a dependency without a version")
+
+		steps.Then("the command exits with an error")
 	})
 
 	steps.Define(func(define Definitions) {
@@ -65,14 +84,40 @@ var _ = Describe("log a dependency", func() {
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		define.When(`^I log a dependency with JSON metadata$`, func() {
+		define.When(`^I log a dependency without a name$`, func() {
+			logCommand := exec.Command(
+				mrlogPath,
+				"dependency",
+				"--version",
+				"1.2.3",
+			)
+
+			var err error
+			commandSession, err = gexec.Start(logCommand, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		define.When(`^I log a dependency without a version$`, func() {
 			logCommand := exec.Command(
 				mrlogPath,
 				"dependency",
 				"--name",
-				"some-name",
+				"marman",
+			)
+
+			var err error
+			commandSession, err = gexec.Start(logCommand, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		define.When(`^I log a dependency with metadata$`, func() {
+			logCommand := exec.Command(
+				mrlogPath,
+				"dependency",
+				"--name",
+				"marman",
 				"--version",
-				"some-version",
+				"1.2.3",
 				"--metadata",
 				"{\"some-key\":\"some-value\"}",
 			)
@@ -90,10 +135,12 @@ var _ = Describe("log a dependency", func() {
 			Eventually(commandSession).Should(gexec.Exit(1))
 		})
 
-		define.Then(`^the result is a machine and human readable dependency log line$`, func() {
+		define.Then(`^the result contains a human readable log$`, func() {
 			Eventually(commandSession.Out).Should(
 				Say("dependency: 'marman' version '1.2.3'"))
+		})
 
+		define.Then(`^the result contains a machine readable log$`, func() {
 			contents := commandSession.Out.Contents()
 
 			mrRE := regexp.MustCompile(`\s(?m)MRL:(.*)\n`)
@@ -102,10 +149,11 @@ var _ = Describe("log a dependency", func() {
 			machineReadableMatches := mrRE.FindSubmatch(contents)
 
 			machineReadable := &struct {
-				Type    string `json:"type"`
-				Name    string `json:"name"`
-				Version string `json:"version"`
-				Time    time.Time
+				Type     string `json:"type"`
+				Name     string `json:"name"`
+				Version  string `json:"version"`
+				Metadata string `json:"metadata"`
+				Time     time.Time
 			}{}
 
 			err := json.Unmarshal(machineReadableMatches[1], machineReadable)
@@ -117,7 +165,7 @@ var _ = Describe("log a dependency", func() {
 			Expect(machineReadable.Time.Unix()).To(BeNumerically("~", time.Now().Unix(), 2))
 		})
 
-		define.Then(`^the resulting machine readable dependency log line contains provided metadata$`, func() {
+		define.Then(`^the machine readable dependency log contains provided metadata$`, func() {
 			contents := commandSession.Out.Contents()
 
 			mrRE := regexp.MustCompile(`\s(?m)MRL:(.*)\n`)
